58aa9e7387f18376317152ab48ae97d6262a224e
mchellmer <mchellmer@gmail.com>
2024-06-08 14:27:24 +0100

    ansible: dhcp

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,3 +3,6 @@ apt upgrade -y
 apt -y install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt -y install ansible
+
+git config --global user.email "mchellmer@gmail.com"
+git config --global user.name "Mark Hellmer"
