ba176830bcc89ac46d090b9ea6d54cf87829867a
mchellmer <mchellmer@gmail.com>
2024-06-08 16:12:23 +0100

    ansible: set ssh password

--- k8s-netplan.sh
+++ k8s-netplan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-ansible-playbook k8s-netplan.yaml -e ENV_WIFI_PASSWORD=$1 -K
+read -p "Enter WiFi password: " wifi_password
+ansible-playbook k8s-netplan.yaml -e ENV_WIFI_PASSWORD=$wifi_password -K
--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-ansible-playbook k8s-nodes.yaml -K -e ansible_user=mchellmer --ask-pass
+read -p "Enter the filepath to the SSH key: " ssh_key_filepath
+ansible-playbook k8s-nodes.yaml -K -e ansible_user=mchellmer -e SSH_KEY_PATH=$ssh_key_filepath --ask-pass
