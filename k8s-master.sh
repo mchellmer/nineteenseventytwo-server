a9cab3e3aaa09e70f772fdd679119d4542d9d978
mchellmer <mchellmer@gmail.com>
2024-06-08 11:37:55 +0100

    ansible: hostname

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook k8s-master.yaml -e "ENV_WIFI_PASSWORD=$1" -e ansible_become_password=$3
+ansible-playbook k8s-master.yaml -e "ENV_WIFI_PASSWORD=$1" -e ansible_become_password=$2
