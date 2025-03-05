e1995d038cd86bab2d1fc94ff0f965786e4d33ab
mchellmer <mchellmer@gmail.com>
2025-03-05 07:57:59 +0000

    docker: update init script with ansible option

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,40 @@
+#!/bin/bash
+
+# This script updates and upgrades the system, installs necessary packages,
+# and optionally installs Ansible if the -a flag is provided.
+#
+# Usage:
+#   ./init.sh [-a]
+#
+# Options:
+#   -a    Install Ansible
+
+# Check if the user wants to install Ansible
+INSTALL_ANSIBLE=false
+while getopts "a" opt; do
+  case ${opt} in
+    a )
+      INSTALL_ANSIBLE=true
+      ;;
+    \? )
+      echo "Usage: cmd [-a]"
+      exit 1
+      ;;
+  esac
+done
+
 apt update
 apt upgrade -y
 apt -y install software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
-apt -y install ansible
+
+if [ "$INSTALL_ANSIBLE" = true ]; then
+  add-apt-repository --yes --update ppa:ansible/ansible
+  apt -y install ansible
+fi
 
 git config --global user.email "mchellmer@gmail.com"
 git config --global user.name "Mark Hellmer"
 
 sed -i 's/^FONTSIZE=.*/FONTSIZE="16x32"/' /etc/default/console-setup
-sed -i 's/^XKBLAYOUT=.*/XKBLAYOUT="gb"/' /etc/default/keyboard
+#sed -i 's/^XKBLAYOUT=.*/XKBLAYOUT="gb"/' /etc/default/keyboard
 reboot now
