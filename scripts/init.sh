55be8721b8ae95dbe181e06fa413f82c5faedf59
mchellmer <mchellmer@gmail.com>
2025-03-17 16:14:22 +0000

    cni: make init applies sudo on appropriate steps

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt -y install software-properties-common sshpass
+sudo apt update
+sudo apt upgrade -y
+sudo apt -y install software-properties-common sshpass
 
-apt -y install ansible
+sudo apt -y install ansible
 
 git config --global user.email "mchellmer@gmail.com"
 git config --global user.name "Mark Hellmer"
 
-sed -i 's/^FONTSIZE=.*/FONTSIZE="16x32"/' /etc/default/console-setup
+sudo sed -i 's/^FONTSIZE=.*/FONTSIZE="16x32"/' /etc/default/console-setup
 
 reboot now
