92c2f9a4333d5b7159ecd1b8443bc7be1bd0685c
mchellmer <mchellmer@gmail.com>
2025-04-14 16:28:54 +0100

    cicd: github actions self hosted runner replacing gitlab - arm64

--- scripts/github-runner.sh
+++ scripts/github-runner.sh
@@ -2,9 +2,10 @@
 
 # Variables
 RUNNER_VERSION="2.323.0"
-RUNNER_URL="https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz"
+RUNNER_URL="https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz"
 RUNNER_FOLDER="$HOME/actions-runner"
 REPO_URL="https://github.com/mchellmer/nineteenseventytwo-server"
+HASH="9cb778fffd4c6d8bd74bc4110df7cb8c0122eb62fda30b389318b265d3ade538"
 
 # Install prerequisites
 echo "Installing prerequisites..."
@@ -20,15 +21,15 @@ mkdir -p $RUNNER_FOLDER && cd $RUNNER_FOLDER
 
 # Download the latest runner package
 echo "Downloading GitHub Actions Runner version $RUNNER_VERSION..."
-curl -o actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz -L $RUNNER_URL
+curl -o actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz -L $RUNNER_URL
 
 # Optional: Validate the hash
 echo "Validating the hash..."
-echo "e3bbb53d4d97859fc1c5cfd9da2a5501686041cbda7c0bb94185e2cde0e9ff1b  actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c
+echo "${HASH}  actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz" | shasum -a 256 -c
 
 # Extract the installer
 echo "Extracting the runner package..."
-tar xzf ./actions-runner-linux-arm-${RUNNER_VERSION}.tar.gz
+tar xzf ./actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz
 
 # Configure the runner
 echo "Configuring the GitHub Actions Runner..."
